d01247a550909b4e759b8fa092f33f33aa3f3661
Denis Herrera Torres <denis.herrera@ucrso.info>
2021-04-12 18:53:48 -0600

    Filter party by region

--- pages/partys.tsx
+++ pages/partys.tsx
@@ -1,26 +1,29 @@
 import firebase from "../services/firebase";
+import regions from "../data/regions.json";
+import searchStyles from "../styles//components/Search.module.scss";
 import { useCollection } from "react-firebase-hooks/firestore";
 import { Container, Row, Col } from "react-bootstrap";
 import { useEffect, useState } from "react";
 import Party from "../components/Party";
 import Spinner from "react-bootstrap/Spinner";
 
-function toTimestamp() {
-  const CURRENT_HOUR = new Date();
-  CURRENT_HOUR.setHours(CURRENT_HOUR.getHours() - 1);
-  return CURRENT_HOUR.getTime();
-}
-
-const query = firebase
-  .firestore()
-  .collection("lkfteam")
-  .where("timestamp", ">", toTimestamp())
-  .orderBy("timestamp", "desc")
-  .limit(10);
-
 const Partys: React.FC = () => {
-  const [querySnapshot, loading, error] = useCollection(query);
   const [teams, setTeams] = useState([]);
+  const [region, setRegion] = useState("LAN");
+
+  const [querySnapshot, loading, error] = useCollection(
+    firebase
+      .firestore()
+      .collection("lkfteam")
+      .where("region", "==", region)
+      .where("active", "==", true)
+      .orderBy("timestamp", "desc")
+      .limit(10)
+  );
+
+  const handleSelect = (value: string) => {
+    setRegion(value);
+  };
 
   useEffect(() => {
     const newTeams = [];
@@ -33,11 +36,36 @@ const Partys: React.FC = () => {
   return (
     <Container>
       <h4 className="my-5 text-center"> GRUPOS DISPONIBLES </h4>
+      <div className={searchStyles.search__field}>
+        <p className="m-5">Ver: </p>
+
+        <div className={`${searchStyles.region__selector}`}>
+          <div className={`${searchStyles.dropbtn}`}>
+            <p> {region} </p>
+            <div
+              id="dropdown__content"
+              className={`${searchStyles.dropdown__content}`}
+            >
+              {Object.keys(regions).map((value: string, i) => {
+                return (
+                  <a key={i} onClick={() => handleSelect(value)}>
+                    {value}
+                  </a>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+      </div>
       <Row>
         {error ? (
           <h4 className="text-center"> Lo sentimos, ha ocurrido un error...</h4>
         ) : loading ? (
-          <Spinner className="m-auto" animation="border" />
+          <Spinner className="m-auto mt-5 " animation="border" />
+        ) : querySnapshot.empty ? (
+          <p className="text-center mt-5">
+            No hay grupos disponibles en este momento{" "}
+          </p>
         ) : (
           teams.map((team, index) => {
             return (
