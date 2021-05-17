ee46d9dd51f2149793bf5770c70b49ff7873aaa7
Denis Herrera Torres <denis.herrera@ucrso.info>
2021-05-16 19:19:28 -0600

    Added photos to commands in index

--- components/Command.tsx
+++ components/Command.tsx
@@ -1,18 +1,61 @@
+import { useState } from "react";
+import Image from "next/image";
 import { Col } from "react-bootstrap";
 import commandStyles from "../styles/Command.module.scss";
 
 interface IProps {
   label: string;
   description: string;
+  showTriangle?: boolean;
+  urlImage?: string;
+  widthImg?: number;
+  heightImg?: number;
 }
 
-const Command: React.FC<IProps> = ({ label, description }: IProps) => {
+const Command: React.FC<IProps> = ({
+  label,
+  description,
+  showTriangle,
+  urlImage,
+  widthImg,
+  heightImg,
+}) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleOnClick = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <div className={commandStyles.command__container}>
       <Col md="8" xs="10" lg="6">
         <p className={commandStyles.command__description}>{description}</p>
-        <div className={commandStyles.command__label}>
-          <p> {label} </p>
+
+        <div className={commandStyles.command__background}>
+          <div className={commandStyles.command__label}>
+            <p> {label} </p>
+            {showTriangle ? (
+              <div
+                onClick={handleOnClick}
+                className={
+                  isOpen ? commandStyles.arrow__down : commandStyles.arrow__up
+                }
+              ></div>
+            ) : (
+              ""
+            )}
+          </div>
+
+          {isOpen ? (
+            <Image
+              src={urlImage}
+              alt="command-img"
+              width={widthImg}
+              height={heightImg}
+            />
+          ) : (
+            ""
+          )}
         </div>
       </Col>
     </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -51,13 +51,14 @@ const Home: React.FC = () => {
         <Container>
           <div className={homeStyles.titles}>
             <h2>Commandos</h2>
+
+            <Command
+              label="t! search "
+              description="Notifica a todas los servidores que alguien está buscando equipo. Recomendamos ver la guía en que se encuentra en Partys. "
+            />
             <Command
               label="t! add-region"
               description="Añade una región al servidor de discord. Si se añade NA por ejemplo, se notificará al sevidor cuando alguien perteneciente a NA busque equipo"
-            />
-            <Command
-              label="t! search "
-              description="Notifica a todas los servidores que alguien está buscando equipo "
             />
             <Command
               label="t! delete-region"
@@ -66,22 +67,42 @@ const Home: React.FC = () => {
             <Command
               label="t! regions"
               description="Muestra todas las regiones"
+              showTriangle
+              urlImage="/index/regions.png"
+              widthImg={259}
+              heightImg={660}
             />
             <Command
               label="t! add "
               description="Vincula tu cuenta de League of Legends con tu cuenta de discord. Con esto se podrá mencionar a un usuario de discord en los comandos"
+              showTriangle
+              urlImage="/index/add.png"
+              widthImg={416}
+              heightImg={795}
             />
             <Command
               label="t! soloq"
               description="Muestra sus estadísticas de soloq"
+              showTriangle
+              urlImage="/index/soloq.png"
+              widthImg={508}
+              heightImg={676}
             />
             <Command
               label="t! flex"
               description="Muestra las estadísticas de flex"
+              showTriangle
+              urlImage="/index/flex.png"
+              widthImg={507}
+              heightImg={680}
             />
             <Command
               label="t! lastmatch "
+              showTriangle
               description="Muestra sus estadísticas del juego más reciente"
+              urlImage="/index/lastmatch-flex.png"
+              widthImg={506}
+              heightImg={645}
             />
           </div>
         </Container>
